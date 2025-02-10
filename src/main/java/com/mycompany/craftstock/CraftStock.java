a3706fc75c912f120007f501cd960192636798a5
bertinef <juliefouda@gmail.com>
2025-02-10 16:50:46 +0100

    login form

--- src/main/java/com/mycompany/craftstock/CraftStock.java
+++ src/main/java/com/mycompany/craftstock/CraftStock.java
@@ -1,19 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.craftstock;
 
 import javax.swing.SwingUtilities;
-/**
- *
- * @author MS GROUP
- */
+
 public class CraftStock {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Login LoginFrame=new Login();
+       LoginFrame.setVisible(true);
+       LoginFrame.pack();
+       LoginFrame.setLocationRelativeTo(LoginFrame);
     }
-    int a;
+    
 }
 
