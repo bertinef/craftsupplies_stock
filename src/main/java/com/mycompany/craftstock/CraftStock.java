b44aa34f4fbcc4621b8bdeab275c254891369248
bertinef <juliefouda@gmail.com>
2025-02-10 14:21:53 +0100

    my first commit

---
+++ src/main/java/com/mycompany/craftstock/CraftStock.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.craftstock;
+
+import javax.swing.SwingUtilities;
+/**
+ *
+ * @author MS GROUP
+ */
+public class CraftStock {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
